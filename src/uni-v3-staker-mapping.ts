66f384e516597dd9994ecc1bf75e2ebfa9f95021
ChenJing <hao286242341@gmail.com>
2024-05-13 18:19:55 +0800

    update

--- src/uni-v3-staker-mapping.ts
+++ src/uni-v3-staker-mapping.ts
@@ -8,37 +8,59 @@ import {
 import {Incentive, Position} from '../generated/schema';
 
 
-
-
 export function handleTokenStaked(event: TokenStaked): void {
   let entity = Position.load(event.params.tokenId.toHex());
+  let incentive = Incentive.load(BigInt.fromI32(1).toHex());
+  if (incentive == null) {
+    incentive = new Incentive(BigInt.fromI32(1).toHex());
+    incentive.liquidity = BigInt.fromI32(0);
+  }
   if (entity != null) {
     entity.staked = true;
     entity.liquidity = event.params.liquidity;
     entity.incentiveId = event.params.incentiveId;
+    incentive.liquidity = entity.liquidity.plus(entity.liquidity);
+    incentive.save();
     entity.save();
   }
 }
 
 export function handleTokenUnstaked(event: TokenUnstaked): void {
   let entity = Position.load(event.params.tokenId.toHex());
+  let incentive = Incentive.load(BigInt.fromI32(1).toHex());
+  if (incentive == null) {
+    incentive = new Incentive(BigInt.fromI32(1).toHex());
+    incentive.liquidity = BigInt.fromI32(0);
+  }
   if (entity != null) {
     entity.staked = false;
+    incentive.liquidity = incentive.liquidity.minus(entity.liquidity);
+
     entity.liquidity = BigInt.fromI32(0);
     entity.incentiveId = null;
     entity.save();
+    incentive.save();
   }
+
 }
 
 export function handleDepositTransferred(event: DepositTransferred): void {
+  let incentive = Incentive.load(BigInt.fromI32(1).toHex());
+  if (incentive == null) {
+    incentive = new Incentive(BigInt.fromI32(1).toHex());
+    incentive.liquidity = BigInt.fromI32(0);
+    incentive.save();
+  }
   let entity = Position.load(event.params.tokenId.toHex());
   if (entity == null) {
     entity = new Position(event.params.tokenId.toHex());
     entity.tokenId = event.params.tokenId;
     entity.owner = event.params.newOwner;
     entity.staked = false;
+    entity.liquidity = BigInt.fromI32(0);
     entity.save();
   }else {
     store.remove("Position", event.params.tokenId.toHex());
   }
 }
+
